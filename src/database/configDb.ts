f67c472e45b345923949da94c24076cc5ad36685
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-08-21 22:11:48 +0200

    feat: refactor readme

--- src/database/configDb.ts
+++ src/database/configDb.ts
@@ -1,21 +1,21 @@
-// import { config } from 'dotenv';
-// import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
-// config();
-// const conf = new ConfigService();
+config();
+const conf = new ConfigService();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 4040,
-  username: 'test',
-  password: 'test',
-  database: 'test_db',
+  host: conf.get('POSTGRESS_HOST'),
+  port: conf.get('POSTGRESS_PORT'),
+  username: conf.get('POSTGRES_USER'),
+  password: conf.get('POSTGRESS_PASSWORD'),
+  database: conf.get('POSTGRESS_DB'),
   migrations: ['dist/migration/*{.ts,.js}'],
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrationsTableName: 'migration',
   migrationsRun: false,
   synchronize: false,
-  logging: false,
+  logging: true,
 };
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
